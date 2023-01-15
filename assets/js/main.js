811d4fe6f8c09fc8cdb1147d04023c3043a833b3
shawnduong <me@shawnd.xyz>
2023-01-15 05:24:06 -0800

    Implemented locality presets.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
+/* From: /assets/json/presets_locality.json */
+var presetsLocality = {};
+
 /* Populate options from JSON. */
 $(document).ready(function()
 {
+	/* Populate locality presets. */
+	$.getJSON("/assets/json/presets_locality.json", function(data)
+	{
+		presetsLocality = data;
+		$.each(data, function(i, item)
+		{
+			$("#preset_locality").append($("<option></option>").val(item.name).html(item.name));
+		});
+	});
+
 	/* Populate timezone options. */
 	$.getJSON("/assets/json/timezones.json", function(data)
 	{
@@ -18,4 +31,22 @@ $(document).ready(function()
 			$("#s_locale").append($("<option></option>").val(item).html(item));
 		});
 	});
+});
+
+/* Handle locality presetting. */
+$("#preset_locality").change(function()
+{
+	if ($(this).val() != "Custom")
+	{
+		var preset = presetsLocality.filter(obj => { return obj.name == $(this).val() })[0];
+		$("#s_timezone").val(preset.timezone).change();
+		$("#s_locale").val(preset.locale).change();
+		$("#s_timezone").attr("disabled", true);
+		$("#s_locale").attr("disabled", true);
+	}
+	else
+	{
+		$("#s_timezone").attr("disabled", false);
+		$("#s_locale").attr("disabled", false);
+	}
 });
