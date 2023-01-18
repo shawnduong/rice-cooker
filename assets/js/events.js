70c9a3a0b169595fcd5e879873074b83a18f87bb
Shawn Duong <shawnduong@protonmail.com>
2023-01-18 10:15:54 -0800

    Refactored JS.

--- assets/js/events.js
+++ assets/js/events.js
@@ -82,40 +82,7 @@ $("#home_remainder").click(function()
 		$("#s_size_home").attr("disabled", false);
 	}
 });
-$("#preset_disk_size").on("input", function()
-{
-	let value = $(this).val();
-	if (value < 16)
-	{
-		$("#s_size_boot").val("").change();
-		$("#s_size_swap").val("").change();
-		$("#s_size_root").val("").change();
-	}
-	else if (value < 32)
-	{
-		$("#s_size_boot").val("512").change();
-		$("#s_size_swap").val("1").change();
-		$("#s_size_root").val("8").change();
-	}
-	else if (value < 64)
-	{
-		$("#s_size_boot").val("512").change();
-		$("#s_size_swap").val("1").change();
-		$("#s_size_root").val("16").change();
-	}
-	else if (value < 128)
-	{
-		$("#s_size_boot").val("512").change();
-		$("#s_size_swap").val("1").change();
-		$("#s_size_root").val("32").change();
-	}
-	else
-	{
-		$("#s_size_boot").val("512").change();
-		$("#s_size_swap").val("1").change();
-		$("#s_size_root").val("64").change();
-	}
-});
+$("#preset_disk_size").on("input", function(){ recommend_disk($(this).val()); });
 
 /* Handle driver type option based on display selection. */
 $("#s_display_intel").click(function()
@@ -156,16 +123,20 @@ $("#s_display_none").click(function()
 	$("#drivers-toggle").addClass("disabled");
 });
 
-/* Help menu. */
+/* Help menu. Handle (?) click. */
 $(".help").click(function(e)
 {
+	/* If the same (?) is clicked again, close it. */
 	if (helpItem == this.parentElement.htmlFor)
 	{
 		$("#help-menu").fadeOut("slow", function() { $("#help-menu").addClass("hidden") });
 		helpItem = "";
 	}
+	/* Bring up the new help menu. */
 	else
 	{
+		/* If there is already no help menu, fade it in. Else, slide the old
+		   menu to the new position. */
 		if ($("#help-menu").hasClass("hidden"))
 		{
 			$("#help-menu").css("top", e.pageY - 64);
@@ -177,12 +148,13 @@ $(".help").click(function(e)
 			$("#help-menu").animate({top: e.pageY-64});
 		}
 
+		/* Fill in the help text. */
 		helpItem = this.parentElement.htmlFor;
 		$("#help-menu-bar-title").text("Help: "+this.parentElement.textContent.slice(0, -4));
-
 		$("#help-menu-body").load("/assets/html/help/"+helpItem+".html");
 	}
 });
+/* Handle X icon click. */
 $("#help-menu-bar-close").click(function()
 {
 	$("#help-menu").fadeOut("slow", function() { $("#help-menu").addClass("hidden") });
@@ -190,43 +162,13 @@ $("#help-menu-bar-close").click(function()
 });
 
 /* Handle None rice configs. */
-$("#s_wmde").change(function()
-{
-	handle_none(this, "#wmde-toggle", "#s_wmde_config", "#s_wmde_config_script");
-});
-$("#s_greeter").change(function()
-{
-	handle_none(this, "#greeter-toggle", "#s_greeter_config", "#s_greeter_config_script");
-});
-$("#s_compositor").change(function()
-{
-	handle_none(this, "#compositor-toggle", "#s_compositor_config", "#s_compositor_config_script");
-});
-$("#s_app_launcher").change(function()
-{
-	handle_none(this, "#app_launcher-toggle", "#s_app_launcher_config", "#s_app_launcher_config_script");
-});
-$("#s_status_bar").change(function()
-{
-	handle_none(this, "#status_bar-toggle", "#s_status_bar_config", "#s_status_bar_config_script");
-});
-$("#s_terminal").change(function()
-{
-	handle_none(this, "#terminal-toggle", "#s_terminal_config", "#s_terminal_config_script");
-});
-$("#s_editor").change(function()
-{
-	handle_none(this, "#editor-toggle", "#s_editor_config", "#s_editor_config_script");
-});
-$("#s_lockscreen").change(function()
-{
-	handle_none(this, "#lockscreen-toggle", "#s_lockscreen_config", "#s_lockscreen_config_script");
-});
-$("#s_wallpaper").change(function()
-{
-	handle_none(this, "#wallpaper-toggle", "#s_wallpaper_config", "#s_wallpaper_config_script");
-});
-$("#s_browser").change(function()
-{
-	handle_none(this, "#browser-toggle", "#s_browser_config", "#s_browser_config_script");
-});
+$("#s_wmde").change(function(){ handle_none(this, "#wmde-toggle", "#s_wmde_config", "#s_wmde_config_script"); });
+$("#s_greeter").change(function(){ handle_none(this, "#greeter-toggle", "#s_greeter_config", "#s_greeter_config_script"); });
+$("#s_compositor").change(function(){ handle_none(this, "#compositor-toggle", "#s_compositor_config", "#s_compositor_config_script"); });
+$("#s_app_launcher").change(function(){ handle_none(this, "#app_launcher-toggle", "#s_app_launcher_config", "#s_app_launcher_config_script"); });
+$("#s_status_bar").change(function(){ handle_none(this, "#status_bar-toggle", "#s_status_bar_config", "#s_status_bar_config_script"); });
+$("#s_terminal").change(function(){ handle_none(this, "#terminal-toggle", "#s_terminal_config", "#s_terminal_config_script"); });
+$("#s_editor").change(function(){ handle_none(this, "#editor-toggle", "#s_editor_config", "#s_editor_config_script"); });
+$("#s_lockscreen").change(function(){ handle_none(this, "#lockscreen-toggle", "#s_lockscreen_config", "#s_lockscreen_config_script"); });
+$("#s_wallpaper").change(function(){ handle_none(this, "#wallpaper-toggle", "#s_wallpaper_config", "#s_wallpaper_config_script"); });
+$("#s_browser").change(function(){ handle_none(this, "#browser-toggle", "#s_browser_config", "#s_browser_config_script"); });
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -101,6 +101,41 @@ function handle_none(object, toggle, config, script)
 	}
 }
 
+/* Recommend disk sizes. */
+function recommend_disk(value)
+{
+	if (value < 16)
+	{
+		$("#s_size_boot").val("").change();
+		$("#s_size_swap").val("").change();
+		$("#s_size_root").val("").change();
+	}
+	else if (value < 32)
+	{
+		$("#s_size_boot").val("512").change();
+		$("#s_size_swap").val("1").change();
+		$("#s_size_root").val("8").change();
+	}
+	else if (value < 64)
+	{
+		$("#s_size_boot").val("512").change();
+		$("#s_size_swap").val("1").change();
+		$("#s_size_root").val("16").change();
+	}
+	else if (value < 128)
+	{
+		$("#s_size_boot").val("512").change();
+		$("#s_size_swap").val("1").change();
+		$("#s_size_root").val("32").change();
+	}
+	else
+	{
+		$("#s_size_boot").val("512").change();
+		$("#s_size_swap").val("1").change();
+		$("#s_size_root").val("64").change();
+	}
+}
+
 /* Collate all form results. */
 function cook()
 {
