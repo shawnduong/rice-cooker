85c6efc26a645a273568ad14b5d013493c1602f9
shawnduong <me@shawnd.xyz>
2023-08-06 23:54:54 -0700

    Reformatted code.

--- frontend/src/components/Form/Personal.vue
+++ frontend/src/components/Form/Personal.vue
@@ -12,37 +12,37 @@ const s_home_style = ref()
 
 <template>
 
-	<div id="personal">
+<div id="personal">
 
-		<h2>Personal</h2>
+	<h2>Personal</h2>
 
-		<label for="s_username" class="big-label">
-			<span>Username </span><a>(?)</a>
-		</label>
-		<input type="text" id="s_username" />
+	<label for="s_username" class="big-label">
+		<span>Username </span><a>(?)</a>
+	</label>
+	<input type="text" id="s_username" />
 
-		<label for="s_hostname" class="big-label">
-			<span>Hostname </span><a>(?)</a>
-		</label>
-		<input type="text" id="s_hostname" />
+	<label for="s_hostname" class="big-label">
+		<span>Hostname </span><a>(?)</a>
+	</label>
+	<input type="text" id="s_hostname" />
 
-		<label class="big-label">
-			<span>Home Style </span><a>(?)</a>
-		</label>
-		<div class="radio-item">
-			<input type="radio" v-model="s_home_style" id="s_home_style_full" value="full" />
-			<label for="s_home_style_full">Full</label>
-		</div>
-		<div class="radio-item">
-			<input type="radio" v-model="s_home_style" id="s_home_style_minimal" value="minimal" />
-			<label for="s_home_style_minimal">Minimal</label>
-		</div>
-		<div class="radio-item">
-			<input type="radio" v-model="s_home_style" id="s_home_style_none" value="none" />
-			<label for="s_home_style_none">None</label>
-		</div>
-
+	<label class="big-label">
+		<span>Home Style </span><a>(?)</a>
+	</label>
+	<div class="radio-item">
+		<input type="radio" v-model="s_home_style" id="s_home_style_full" value="full" />
+		<label for="s_home_style_full">Full</label>
 	</div>
+	<div class="radio-item">
+		<input type="radio" v-model="s_home_style" id="s_home_style_minimal" value="minimal" />
+		<label for="s_home_style_minimal">Minimal</label>
+	</div>
+	<div class="radio-item">
+		<input type="radio" v-model="s_home_style" id="s_home_style_none" value="none" />
+		<label for="s_home_style_none">None</label>
+	</div>
+
+</div>
 
 </template>
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,31 @@
+<!-- Script -->
+
 <script setup lang="ts">
-	import Header from "../components/Header/Header.vue"
-	import Personal from "../components/Form/Personal.vue"
-	import Finish from "../components/Finish/Finish.vue"
+
+import Header from "../components/Header/Header.vue"
+import Personal from "../components/Form/Personal.vue"
+import Finish from "../components/Finish/Finish.vue"
+
 </script>
 
+<!-- Template -->
+
 <template>
-	<Header />
-	<div id="main">
-		<Personal /><hr>
-		<Finish />
-	</div>
+
+<Header />
+<div id="main">
+	<Personal /><hr>
+	<Finish />
+</div>
+
 </template>
+
+<!-- Style -->
+
+<style scoped>
+
+#main {
+	margin: 60px 15%;
+}
+
+</style>
