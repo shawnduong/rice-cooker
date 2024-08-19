3c8a3d1d7728dd41a8c26283bc20f2eeae5f63aa
shawnduong <me@shawnd.xyz>
2024-08-18 22:50:26 -0700

    Sileneced network enabling.

--- base-install/modules/secondary_chroot/enable_network.sh
+++ base-install/modules/secondary_chroot/enable_network.sh
@@ -3,6 +3,6 @@ source modules/common/flag.sh
 # Enable NetworkManager so that networking works. Flags 2011.
 enable_network()
 {
-	systemctl enable NetworkManager
+	systemctl enable NetworkManager >/dev/null
 	flag 2011
 }
